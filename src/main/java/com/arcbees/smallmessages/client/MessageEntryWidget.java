82c76c50d973c1ef67c6c4455d803045584cbd14
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-08-04 18:31:40 -0400

    Add behaviour on click of the button.

--- src/main/java/com/arcbees/smallmessages/client/MessageEntryWidget.java
+++ src/main/java/com/arcbees/smallmessages/client/MessageEntryWidget.java
@@ -1,10 +1,13 @@
 package com.arcbees.smallmessages.client;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.*;
 
-public class MessageEntryWidget implements IsWidget, KeyUpHandler {
+public class MessageEntryWidget implements IsWidget, KeyUpHandler, ClickHandler {
     private final String CHARACTERS_LEFT = "Characters left : ";
     private final int MAX_CHARACTERS = 150;
 
@@ -16,6 +19,7 @@ public class MessageEntryWidget implements IsWidget, KeyUpHandler {
 
     public MessageEntryWidget() {
         publishButton.setText("Publish");
+        publishButton.addClickHandler(this);
 
         messageText.getElement().setPropertyString("placeholder", "What are you thinking about?");
         messageText.addKeyUpHandler(this);
@@ -39,5 +43,29 @@ public class MessageEntryWidget implements IsWidget, KeyUpHandler {
 
     private void updateCount() {
         charactersLeft.setText(CHARACTERS_LEFT + String.valueOf(MAX_CHARACTERS - messageText.getText().length()));
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        submitMessage();
+    }
+
+    private void submitMessage() {
+        if(isValidMessage()) {
+            addMessage(messageText.getText());
+            messageText.setText("");
+            updateCount();
+        } else {
+            Window.alert("Your message is invalid. Please enter between 1 and "
+                    + String.valueOf(MAX_CHARACTERS) + " characters.");
+        }
+    }
+
+    private void addMessage(String text) {
+        verticalPanel.add(new MessageWidget(text));
+    }
+
+    private boolean isValidMessage() {
+        return messageText.getText().length() <= MAX_CHARACTERS && messageText.getText().length() > 0;
     }
 }
