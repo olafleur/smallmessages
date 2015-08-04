0eb028c6339abb7dfdb429b026d736621c656829
Olivier Lafleur <olivier.lafleur@gmail.com>
2015-08-04 19:10:11 -0400

    Add an image.

--- src/main/java/com/arcbees/smallmessages/client/resources/Resources.java
+++ src/main/java/com/arcbees/smallmessages/client/resources/Resources.java
@@ -3,6 +3,7 @@ package com.arcbees.smallmessages.client.resources;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
 
 public interface Resources extends ClientBundle {
     Resources INSTANCE = GWT.create(Resources.class);
@@ -14,4 +15,7 @@ public interface Resources extends ClientBundle {
     }
 
     Styles styles();
+
+    @Source("logo.jpg")
+    ImageResource logo();
 }
